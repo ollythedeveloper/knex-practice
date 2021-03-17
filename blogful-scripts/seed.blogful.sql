9b9c6e45b02144a8f777b9dbfec3997a90a0a1cb
Olivia Lewis <ollybaby12@gmail.com>
2021-03-17 15:57:08 -0400

    add blogful scripts

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('The truth behind every argument', now() - '29 days'::INTERVAL, 'Learning to release truths before they become weapons.'),
+    ('When is a good time to buy a house', now() - '28 days'::INTERVAL, 'Are you ready to invest in your first home?'),
+    ('Letting go of toxic relationships', now() - '27 days'::INTERVAL, 'Healthy way to release toxic people from your life.'),
+    ('Working through creative blocks', now() - '26 days'::INTERVAL, 'Finding ways to reawaken your creative nature'),
+    ('Finding a muse', now() - '25days'::INTERVAL, 'How do you know you have the muse you need?'),
+    ('Parenting in a Covid World', now() - '24 days'::INTERVAL, 'Mainting a safe and clean environment for your kids'),
+    ('10 Stylish hairstyles that show you''re in charge', now() - '23 days'::INTERVAL, 'How to look proffessional without sacrficing your unique stlye'),
+    ('Five Innovative Approaches To Improve Your Workout', now() - '22 days'::INTERVAL, 'Get mor out of your workouts. See results quicker.'),
+    ('The Death Of Breakfast', now() - '21 days'::INTERVAL, 'Is breakfast becoming a meal of the past? What does this mean for our mornings?'),
+    ('20 Myths About Sleep: Busted', now() - '20 days'::INTERVAL, 'Are the myths about sleep stopping you from getting...sleep?'),
+    ('The real reason your schedule doesn''t work', now() - '19 days'::INTERVAL, 'Find ways to make a better schedule and stick to it!' ),
+    ('Why Eating Healthy Isn''t Working for You', now() - '18 days'::INTERVAL, 'A deep dive into what you''re actually eating'),
+    ('Why Friends Will Change Your Life', now() - '17 days'::INTERVAL, 'The impact of the people you surround yourself with'),
+    ('Where Camping is Headed in the Next Three Years', now() - '16 days'::INTERVAL, 'All the perks of GLAMPing!'),
+    ('9 Podcast About Personal Finances', now() - '15 days'::INTERVAL, 'Learn godd money habits during your commute'),
+    ('The Unconventional Guide to Decorating Your Home', now() - '14 days'::INTERVAL, 'Make your home about you and what makes you feel at home'),
+    ('What Experts Are Saying About Going Back to School', now() - '13 days'::INTERVAL, 'Is going back to school worth it?'),
+    ('The Era of Comfort Style', now() - '12 days'::INTERVAL, 'Feeling at home when at work'),
+    ('Fitting Meal Prep Into Your Busy Life', now() - '11 days'::INTERVAL, 'Stop getting take out everyday. Make time for a healthier lifestyle'),
+    ('Improve Yout Lifestyle in 3 Weeks', now() - '10 days'::INTERVAL, 'It really will only take 3 weeks!!')
+;
+
